daa192ad7e2746849824ca3894e1a040b1a5c317
gruiz17 <gruiz17@berkeley.edu>
2014-10-20 16:50:35 -0700

    menu stuff, architecture decisions

--- clouded/models/song.js
+++
@@ -1,9 +0,0 @@
-var Song = Backbone.Model.extend({
-  defaults: {
-    name: "not specified",
-    artist: "not specified"
-  },
-  initialize: function() {
-    console.log("sample API call replacing name and artist");
-  }
-})
